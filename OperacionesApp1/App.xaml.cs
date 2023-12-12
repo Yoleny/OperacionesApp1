697eec397ecfaf31ea987f6824787f220101aff6
Yoleny <yoleny.salme28@gmail.com>
2023-12-11 19:24:59 -0600

    Agregar archivos de proyecto.

---
+++ OperacionesApp1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Application = Microsoft.Maui.Controls.Application;
+
+namespace OperacionesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new OperacionesApp1.MainPage();
+
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OperacionesApp1/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace OperacionesApp1
+{
+    public partial class App : Aplication
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
